e0fcb27798f5ef07f83677d035773376f58de2ca
Ziying Pan <taiqinwu1999@gmail.com>
2022-09-09 21:40:57 -0700

    Map Liine

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,11 +1,11 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
+// Create the map object with a center and zoom level. What you see when you open the page
 let map = L.map('mapid').setView([40.7, -95.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -13,14 +13,18 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Add a market to the map
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Coordinates for each point to be used in the line.
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6417, -73.7809]
+  ];
 
-// Add a circle to the map
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    // color of the circle line
-    color: "black",
-    // color of the circle inside
-    fillColor: '#ffffa1'
- }).addTo(map);
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: 'blue', 
+    weight: '2', 
+    dashArray: '5, 5'
+}).addTo(map);
+
